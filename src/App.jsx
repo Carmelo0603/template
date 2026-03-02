a17dcc57da32071bac18e9e0fee135977f70c8a6
Carmelo0603 <carmeloprovenzani1@gmail.com>
2026-03-02 23:09:40 +0100

    prova epibooks nuovo da capo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import MyNav from "./components/MyNav";
+
+function App() {
+  return (
+    <>
+      <MyNav></MyNav>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/MyNav.jsx
@@ -0,0 +1,18 @@
+import { Nav, Container } from "react-bootstrap";
+
+function MyNav() {
+  return (
+    <Navbar>
+      <Container>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default MyNav;
